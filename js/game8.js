c2f39256c64d394583df31b14fa4f81345cac0c8
vincent.lin <vincent.lin@ubitusinc.com>
2017-12-12 21:13:12 +0800

    replace header and add dictionary

--- js/game8.js
+++ js/game8.js
@@ -1,9 +1,34 @@
+(function(){
+  if(location.href.indexOf('fe-heroes') < 0 ){
+    return;
+  }
+var header = [
+['リセマラ','刷首抽'],
+['ランキング','排行'],
+['キャラクター','角色'],
+['スキル','技能'],
+['ガチャ','抽卡'],
+['イベント','活動']
+];
 var dictionary = [
 ['リセマラ','刷首抽'],
 ['ランキング','排行'],
+['キャラ','角色'],
+['ステータス','能力值'],
+['おすすめ','推薦'],
+['ラインハルト','萊茵哈特'],
+['リン','琳'],
 ];
+var pageNav = document.querySelector('.p-gameNavText').innerHTML;
+header.forEach(function(word){
+  pageNav = pageNav.replace(new RegExp(word[0],'g'), word[1]);
+});
+document.querySelector('.p-gameNavText').innerHTML = pageNav;	
 var pageContent = document.querySelector('.l-content').innerHTML;
-dictionary.forEach(function(word){
+header.concat(dictionary).forEach(function(word){
   pageContent = pageContent.replace(new RegExp(word[0],'g'), word[1]);
 });
-document.querySelector('.l-content').innerHTML = pageContent;
+document.querySelector('.l-content').innerHTML = pageContent;	
+	
+})();
+
